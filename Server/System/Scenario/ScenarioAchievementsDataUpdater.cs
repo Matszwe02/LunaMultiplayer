32567ffbae600c5be3cbe5794cd291aa780043b9
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-09-27 23:26:38 +0200

    Deduplicate crew members in achievements

--- Server/System/Scenario/ScenarioAchievementsDataUpdater.cs
+++ Server/System/Scenario/ScenarioAchievementsDataUpdater.cs
@@ -1,5 +1,6 @@
 ﻿using LmpCommon.Message.Data.ShareProgress;
 using LunaConfigNode.CfgNode;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,7 @@ namespace Server.System.Scenario
                     {
                         var specificNode = progressNodeHeader.GetNode(achievementMsg.Id);
                         var receivedNode = new ConfigNode(Encoding.UTF8.GetString(achievementMsg.Data, 0, achievementMsg.NumBytes)) { Name = achievementMsg.Id };
+                        DeduplicateCrewInConfigNode(receivedNode);
                         if (specificNode != null)
                         {
                             progressNodeHeader.ReplaceNode(specificNode.Value, receivedNode);
@@ -34,6 +36,26 @@ namespace Server.System.Scenario
                     }
                 }
             });
+        }
+
+        /// <summary>
+        /// Deduplicates crew members within a ConfigNode.
+        /// </summary>
+        private static void DeduplicateCrewInConfigNode(ConfigNode node)
+        {
+            foreach (var subNodeValue in node.GetNodes("crew"))
+            {
+                var subNode = subNodeValue.Value; // subNode is a ConfigNode
+
+                var crewsCfgNodeValue = subNode.GetValue("crews"); // This returns CfgNodeValue<string, string>
+
+                if (crewsCfgNodeValue != null && !string.IsNullOrWhiteSpace(crewsCfgNodeValue.Value))
+                {
+                    var crewsString = crewsCfgNodeValue.Value;
+                    var distinctCrews = crewsString.Split(',').Select(c => c.Trim()).Distinct().Where(c => !string.IsNullOrWhiteSpace(c)).ToList();
+                    crewsCfgNodeValue.Value = string.Join(", ", distinctCrews); // Attempt to set the Value property of CfgNodeValue
+                }
+            }
         }
     }
 }
